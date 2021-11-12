b28fa3d7588e0c4a16c60f1631923a74955c2d4c
David <dmartinezmillan@hotmail.com>
2021-11-12 11:09:43 +0100

    feat: changed versoning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="open-variant",
-    version="0.0.1",
+    version="0.1.0",
     author="BBGLab - Barcelona Biomedical Genomics Lab",
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests', 'plugins']),
     include_package_data=True,
