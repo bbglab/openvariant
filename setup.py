b1cd0af3104d4a2530b2afa97af858c93f79b7e5
David Martinez <david.martinez@irbbarcelona.org>
2021-11-18 17:01:32 +0100

    feat: changed versoning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="open-variant",
-    version="0.2.2",
+    version="0.2.3",
     author="BBGLab - Barcelona Biomedical Genomics Lab",
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests', 'plugins']),
     include_package_data=True,
