22d2c6bf472d1eb48741263f071d9743ff850ccf
David Martinez <david.martinez@irbbarcelona.org>
2022-01-11 11:20:03 +0100

    feat: comments on openvar

--- openvariant/commands/openvar.py
+++ openvariant/commands/openvar.py
@@ -1,6 +1,7 @@
 from os import cpu_count
 import click as click
 
+import openvariant
 from openvariant.commands.tasks.cat import cat as cat_task
 from openvariant.commands.tasks.count import count as count_task
 from openvariant.commands.tasks.groupby import group_by as group_by_task
@@ -8,9 +9,10 @@ from openvariant.commands.tasks.plugin import PluginActions
 
 
 @click.group(context_settings={'help_option_names': ['-h', '--help']})
-# @click.option('--debug', help='Show debug messages.', is_flag=True)
-@click.version_option()
+@click.version_option(openvariant.__version__)
 def main():
+    """'openvar' is the command-line interface of OpenVariant.
+    Parsing and data transformation for multiple input formats."""
     pass
 
 
@@ -20,6 +22,7 @@ def main():
 @click.option('--annotations', '-a', default=None)
 @click.option('--header', help="Show the result header", is_flag=True)
 def cat(input_path: str, where: str or None, annotations: str or None, header: bool):
+    """Print the parsed files on the stdout."""
     cat_task(input_path, annotations, where, header)
 
 
@@ -31,6 +34,7 @@ def cat(input_path: str, where: str or None, annotations: str or None, header: b
 @click.option('--cores', '-c', help='Maximum processes to run in parallel.', type=click.INT, default=cpu_count())
 @click.option('--quite', '-q', help="Don't show the progress, only the total count.", is_flag=True)
 def count(input_path: str, where: str, group_by: str, cores: int, quite: bool, annotations: str or None) -> None:
+    """Print on the stdout the number of rows that meets the criteria."""
     result = count_task(input_path, annotations, group_by=group_by, where=where, cores=cores, quite=quite)
     if len(result[1]) > 0:
         for k, v in sorted(result[1].items(), key=lambda res: res[1]):
@@ -51,6 +55,7 @@ def count(input_path: str, where: str, group_by: str, cores: int, quite: bool, a
 @click.option('--quite', '-q', help="Don't show the progress, only the total count.", is_flag=True)
 def groupby(input_path: str, script: str, where: str, group_by: str, cores: int, quite: bool, annotations: str or None,
             header: bool, show: bool):
+    """Print on the stdout the parsed files group by a specified field."""
     for group_key, group_result, command in group_by_task(input_path, annotations, script, key_by=group_by, where=where,
                                                           cores=cores, quite=quite, header=header):
         for r in group_result:
@@ -64,11 +69,12 @@ def groupby(input_path: str, script: str, where: str, group_by: str, cores: int,
                     print(f"{group_key}\t{r}") if show else print(f"{r}")
 
 
-@main.command(name="plugin", short_help='')
+@main.command(name="plugin", short_help='Actions to execute for a plugin: create')
 @click.argument('action', type=click.Choice(['create']))
 @click.option('--name', '-n', type=click.STRING)
 @click.option('--directory', '-d', type=click.STRING)
 def plugin(action, name: str or None, directory: str or None):
+    """Actions to apply on the plugin system."""
     PluginActions[action.upper()].value(name, directory)
 
 
