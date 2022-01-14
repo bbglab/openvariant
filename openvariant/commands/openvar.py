009b589866ee074ba0c0fce9926d66defb6d760f
David Martinez <david.martinez@irbbarcelona.org>
2022-01-14 17:03:52 +0100

    fix: openvar fix

--- openvariant/commands/openvar.py
+++ openvariant/commands/openvar.py
@@ -10,13 +10,13 @@ from openvariant.commands.tasks.plugin import PluginActions
 
 @click.group(context_settings={'help_option_names': ['-h', '--help']})
 @click.version_option(openvariant.__version__)
-def main():
+def openvar():
     """'openvar' is the command-line interface of OpenVariant.
-    Parsing and data transformation for multiple input formats."""
+    Parsing and data transformation of multiple input formats."""
     pass
 
 
-@main.command(name="cat", short_help='Concatenate files to standard input')
+@openvar.command(name="cat", short_help='Concatenate files to standard input')
 @click.argument('input_path', type=click.Path(exists=True), default='.')
 @click.option('--where', '-w', type=click.STRING, default=None, help="Filter expression. eg: CHROMOSOME == 4")
 @click.option('--annotations', '-a', default=None)
@@ -26,10 +26,10 @@ def cat(input_path: str, where: str or None, annotations: str or None, header: b
     cat_task(input_path, annotations, where, header)
 
 
-@main.command(name="count", short_help='Number of rows that matches a specified criterion')
+@openvar.command(name="count", short_help='Number of rows that matches a specified criterion')
 @click.argument('input_path', type=click.Path(exists=True), default='.')
-@click.option('--where', '-w', multiple=False, type=click.STRING)
-@click.option('--group_by', '-g', type=click.STRING)
+@click.option('--where', '-w', multiple=False, type=click.STRING, help="Filter expression. eg: CHROMOSOME == 4")
+@click.option('--group_by', '-g', type=click.STRING, help="Filter expression. eg: CHROMOSOME")
 @click.option('--annotations', '-a', default=None, type=click.Path(exists=True))
 @click.option('--cores', '-c', help='Maximum processes to run in parallel.', type=click.INT, default=cpu_count())
 @click.option('--quite', '-q', help="Don't show the progress, only the total count.", is_flag=True)
@@ -43,13 +43,14 @@ def count(input_path: str, where: str, group_by: str, cores: int, quite: bool, a
     print("TOTAL\t{}".format(result[0]))
 
 
-@main.command(name="groupby", short_help='Groups rows that have the same values into summary rows')
+@openvar.command(name="groupby", short_help='Groups rows that have the same values into summary rows')
 @click.argument('input_path', type=click.Path(exists=True), default='.')
 @click.option('--header', help='Send header as first row', is_flag=True)
 @click.option('--show', help='Show group by each row', is_flag=True)
-@click.option('--group_by', '-g', type=click.STRING, default=None)
-@click.option('--where', '-w', type=click.STRING, default=None)
-@click.option('--script', '-s', type=click.STRING, default=None)
+@click.option('--group_by', '-g', type=click.STRING, default=None, help="Filter expression. eg: CHROMOSOME")
+@click.option('--where', '-w', type=click.STRING, default=None, help="Filter expression. eg: CHROMOSOME == 4")
+@click.option('--script', '-s', type=click.STRING, default=None,
+              help="Filter expression. eg: gzip > \${GROUP_KEY}.parsed.tsv.gz")
 @click.option('--annotations', '-a', default=None, type=click.Path(exists=True))
 @click.option('--cores', '-c', help='Maximum processes to run in parallel.', type=click.INT, default=cpu_count())
 @click.option('--quite', '-q', help="Don't show the progress, only the total count.", is_flag=True)
@@ -69,7 +70,7 @@ def groupby(input_path: str, script: str, where: str, group_by: str, cores: int,
                     print(f"{group_key}\t{r}") if show else print(f"{r}")
 
 
-@main.command(name="plugin", short_help='Actions to execute for a plugin: create')
+@openvar.command(name="plugin", short_help='Actions to execute for a plugin: create')
 @click.argument('action', type=click.Choice(['create']))
 @click.option('--name', '-n', type=click.STRING)
 @click.option('--directory', '-d', type=click.STRING)
@@ -79,4 +80,4 @@ def plugin(action, name: str or None, directory: str or None):
 
 
 if __name__ == "__main__":
-    main()
+    openvar()
--- tests/test_command/test_help_command.py
+++ tests/test_command/test_help_command.py
@@ -2,14 +2,15 @@ import unittest
 
 from click.testing import CliRunner
 
-from openvariant.commands.openvar import main
+from openvariant.commands.openvar import openvar
 
 
 class TestHelpCommand(unittest.TestCase):
 
     def test_help_command(self):
         runner = CliRunner()
-        result = runner.invoke(main)
+        result = runner.invoke(openvar)
         self.assertTrue("cat      Concatenate files to standard input" in result.output)
         self.assertTrue("count    Number of rows that matches a specified criterion" in result.output)
         self.assertTrue("groupby  Groups rows that have the same values into summary rows" in result.output)
+        self.assertTrue("plugin   Actions to execute for a plugin: create" in result.output)
