89ee6a8798e081a1fc41be315775ca7d29896990
David Martinez <david.martinez@irbbarcelona.org>
2022-03-23 15:07:11 +0100

    fix: changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="open-variant",
-    version="0.6.13",
+    version="0.6.14",
     author="BBGLab - Barcelona Biomedical Genomics Lab",
     packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=True,
