7fa81c6c8499392a3306c1781d58a89adb717f89
CarlosLopezElorduy <carlos.lopez@irbbarcelona.org>
2022-07-01 12:55:14 +0200

    feat: -o --output flag added

--- openvariant/tasks/cat.py
+++ openvariant/tasks/cat.py
@@ -15,8 +15,8 @@ def _format_line(line: List[str], out_format: str) -> str:
     return AnnotationFormat[out_format.upper()].value.join(line)
 
 
-def cat(base_path: str, annotation_path: str or None = None, where: str = None, header_show: bool = True) -> None:
-    """Print on the stdout the parsed files.
+def cat(base_path: str, annotation_path: str or None = None, where: str = None, header_show: bool = True, output: str or None = None) -> None:
+    """Print on the stdout/"output" the parsed files.
 
     It will parse the input files with its proper annotation schema, and it'll show the result on the stdout.
     It can be printed with or without header. Can be added a 'where' expression.
@@ -32,12 +32,21 @@ def cat(base_path: str, annotation_path: str or None = None, where: str = None, 
     header_show : bool
         Shows header on the output.
     """
+    if output:
+        out_file = open(output, "w")
     for file, annotation in find_files(base_path, annotation_path):
         result = Variant(file, annotation)
         header = result.header
         if header_show:
-            print(_format_line(header, result.annotation.format))
+            if output:
+                out_file.write(_format_line(header, result.annotation.format))
+                out_file.write("\n")
+            else: print(_format_line(header, result.annotation.format))
             header_show = False
         for i, r in enumerate(result.read(where=where)):
             if isinstance(r, dict):
-                print(_format_line(list(map(str, r.values())), result.annotation.format))
+                if output:
+                    out_file.write(_format_line(list(map(str, r.values())), result.annotation.format))
+                    out_file.write("\n")
+                else: print(_format_line(list(map(str, r.values())), result.annotation.format))
+    if output: out_file.close()
